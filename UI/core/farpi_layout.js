3b533dab64ec6f8a189de0b13d8ea39923b08aeb
olivermalham <oliver.malham@gmail.com>
2023-03-24 22:54:43 +0000

    Got tabs working, but seem to have borked the RPC mechanism :(

--- UI/core/farpi_layout.js
+++ UI/core/farpi_layout.js
@@ -18,11 +18,7 @@ customElements.define('farpi-navbar', FarPiNavbar);
 class FarPiNavbarButton extends FarPiElement {
     setup() {
         this.logo = this.getAttribute("logo");
-        this.innerHTML =
-            `<button class="btn">
-                ${this.innerHTML}
-            </button>`;
-        // <div class="flex items-center justify-center bg-base-100 text-primary border border-2 border-neutral p-1 mr-3 rounded-xl w-28 h-12 items-center">
+        this.classList.add("tab")
     }
 }
 customElements.define('farpi-navbar-button', FarPiNavbarButton);
@@ -32,10 +28,11 @@ class FarPiTabList extends FarPiElement {
     // Tab headers
     setup() {
         this.innerHTML =
-            `<div class="tabs tabs-boxed mr-3">
+            `<div class="mr-3">
                 ${this.innerHTML}
             </div>`;
-    }
+        farPiTabRefresh("0");
+    } //tabs tabs-boxed
 }
 customElements.define('farpi-tab-list', FarPiTabList);
 
@@ -44,12 +41,40 @@ class FarPiTab extends FarPiElement {
     // Container for the contents of each tab
     setup() {
         this.classList.add("pr-3");
-        this.innerHTML =
-            `<div class="flex items-center justify-center text-neutral border border-2 border-neutral p-1 rounded-xl w-12 h-12 items-center">
-                <div class="text-neutral inline-block h-6 w-6 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status">
-                    
-                </div>
-            </div>`;
+        this.tab_number = this.getAttribute("number");
     }
 }
 customElements.define('farpi-tab', FarPiTab);
+
+
+class FarPiTabBtn extends FarPiElement {
+    // Container for the contents of each tab
+    setup() {
+        this.tab_number = this.getAttribute("tab");
+        this.classList.add("btn", "btn-wide", "h-12", "text-xl")
+        this.onclick = this.onclick_handler
+    }
+
+    onclick_handler(event) {
+        farPiTabRefresh(this.tab_number);
+    }
+}
+customElements.define('farpi-tab-btn', FarPiTabBtn);
+
+
+function farPiTabRefresh(tab_number){
+    // Global function to refresh tab visibility.
+    let tabs = document.getElementsByTagName("farpi-tab");
+    for (let tab in tabs){
+        tabs[tab].hidden = (tabs[tab].tab_number !== tab_number);
+    }
+
+    let tab_btns = document.getElementsByTagName("farpi-tab-btn");
+    for (let btn =0; btn < tab_btns.length; btn++){
+        if (btn == tab_number) {
+            tab_btns[btn].classList.add("bg-success");
+        } else {
+            tab_btns[btn].classList.remove("bg-success");
+        }
+    }
+}
